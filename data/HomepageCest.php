3e8ae8a1ee13d6ce527cdc1cb51a4df232825b8c
balkrishna <balkrishna.shrestha@itoffshorenepal.com>
2023-12-15 09:55:51 +0545

    duplicate function

--- data/HomepageCest.php
+++ data/HomepageCest.php
@@ -17,3 +17,20 @@ class HomepageCest
         $I->see('You\'re viewing the <HelloWorld> component!');
     }       
 }
+class HomepageCest 
+{    
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+    }
+
+    public function hasPageTitle(AcceptanceTester $I)
+    {
+        $I->see('Homepage');
+    }
+    
+    public function vueIsRendered(AcceptanceTester $I)
+    {
+        $I->see('You\'re viewing the <HelloWorld> component!');
+    }       
+}
