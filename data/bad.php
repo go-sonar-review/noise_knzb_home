c6fb3b36c23c8eff895723140507cc898a928940
balkrishna <balkrishna.shrestha@itoffshorenepal.com>
2023-12-15 10:19:06 +0545

    2 updated bad.php

--- data/bad.php
+++ data/bad.php
@@ -6,3 +6,11 @@ echo("<p>Search results for query: " .
 echo("<p>Search results for query: " .
     $_GET['query'] . ".</p>");
 ?>
+<?
+echo("<p>Search results for query: " .
+    $_GET['query'] . ".</p>");
+?>
+<?
+echo("<p>Search results for query: " .
+    $_GET['query'] . ".</p>");
+?>
