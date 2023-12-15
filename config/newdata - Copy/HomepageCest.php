03b06d11fb74589b8ee68f97f476ff363e4e8581
balkrishna <balkrishna.shrestha@itoffshorenepal.com>
2023-12-15 10:30:05 +0545

    added copy of newdata folder in config folder

---
+++ config/newdata - Copy/HomepageCest.php
@@ -0,0 +1,36 @@
+<?php
+
+class HomepageCest 
+{    
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+    }
+
+    public function hasPageTitle(AcceptanceTester $I)
+    {
+        $I->see('Homepage');
+    }
+    
+    public function vueIsRendered(AcceptanceTester $I)
+    {
+        $I->see('You\'re viewing the <HelloWorld> component!');
+    }       
+}
+class HomepageCest 
+{    
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+    }
+
+    public function hasPageTitle(AcceptanceTester $I)
+    {
+        $I->see('Homepage');
+    }
+    
+    public function vueIsRendered(AcceptanceTester $I)
+    {
+        $I->see('You\'re viewing the <HelloWorld> component!');
+    }       
+}
