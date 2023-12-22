67c26d1cd3dadafb1b17df40bf8d978638337e2e
go-sonar-review <153824558+go-sonar-review@users.noreply.github.com>
2023-12-22 16:36:31 +0545

    Update1 bad.php

--- data/bad.php
+++ data/bad.php
@@ -10,7 +10,3 @@ echo("<p>Search results for query: " .
 echo("<p>Search results for query: " .
     $_GET['query'] . ".</p>");
 ?>
-<?
-echo("<p>Search results for query: " .
-    $_GET['query'] . ".</p>");
-?>
